f1e326539c256bb12c113ced049c54f170cc21ff
Subhadip <mnhacker2001@gmail.com>
2024-02-19 20:32:10 +0530

    Remove commented code for widget store

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -56,16 +56,3 @@ const useWidgetStore = create<WidgetStore>((set) => ({
 }));
 
 export { useTabStore, useModalStateStore, useWidgetStore };
-
-// export const useWidgetStore = create(
-//   persist(
-//     (set) => ({
-//       widgets: [],
-//       addWidget: (widget: any) => set((state) => ({ widgets: [...state.widgets, widget] })),
-//       removeWidget: (id: string) => set((state) => ({ widgets: state.widgets.filter((widget: any) => widget.id !== id) })),
-//     }),
-//     {
-//       name: "widget-store",
-//     }
-//   )
-// );
